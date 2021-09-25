284c7abe38c70b5e8431b6316d486b37c71feebc
Icaro Soares - Accenture - TI <i.albuquerque.soares@avanade.com>
2021-09-24 22:31:34 -0300

    Domain modeling

---
+++ src/Avanade.Challenge.Domain/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Avanade.Challenge.Domain
+{
+    public struct Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
